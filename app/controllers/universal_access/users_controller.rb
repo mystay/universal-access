143f09b3b8fc14d66ff65fae28c62b57a3ad17ad
Ben Petro <bpetro@feet.shoes>
2016-10-07 07:32:04 +0000

    users

--- app/controllers/universal_access/users_controller.rb
+++ app/controllers/universal_access/users_controller.rb
@@ -3,6 +3,16 @@ require_dependency "universal_access/application_controller"
 module UniversalAccess
   class UsersController < ApplicationController
 
+    def index
+      #list users who have access to a certain group by the group code
+      @user_group = UniversalAccess::UserGroup.find_by(code: params[:code])
+      users = []
+      if @user_group
+        users = @user_group.users.sort_by{|a| a.name}.map{|u| {name: u.name, email: u.email, first_name: u.name.split(' ')[0].titleize, id: u.id.to_s, functions: u.user_group_functions}}
+      end
+      render json: users      
+    end
+    
     def autocomplete
       @users = Universal::Configuration.class_name_user.classify.constantize.all
       if Universal::Configuration.user_scoped
